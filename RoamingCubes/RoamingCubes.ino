b5a998e3635f707c73db03a2c7d3d413aa5b26e7
Adam Reed <adam@secretcode.ninja>
2015-10-05 18:57:04 +1100

    Instead of waiting for a serial connection, start regardless after 3 seconds, or when the connection is made (which ever happens first). Then during the setup phase, if a serial connection is present, print the sketch name and version, and if not in debug mode, flag that debug is off.

--- RoamingCubes/RoamingCubes.ino
+++ RoamingCubes/RoamingCubes.ino
@@ -93,8 +93,24 @@ void setup(void) {
   // 1: Control via the RXD and TXD pins on the main board.
   // -1: Don't attach any serial port to interact with the Cube.
   cube.begin(0, 115200); // Start on serial port 0 (USB) at 115200 baud
-  while (!Serial) {
+  
+  // Wait for the serial interface, to be established, or for a maximum of
+  // 3 seconds.
+  byte waitCounter = 0;
+  while (waitCounter < 30 && !Serial) {
+    delay(100);
+    waitCounter++;
   }
+
+  // Print Debug Info if a serial interface is present
+  if (Serial) 
+  {
+    serial->println("RoamingCubes v1.0");
+#ifndef DEBUG
+    serial->println("DEBUG Mode off");
+#endif
+  }
+  
   randomSeed(1);
 
   // Create the small cubes, and flag / track where they are
