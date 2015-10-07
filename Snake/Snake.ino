65d6145e14773654563b1425e0a6e642416d88f4
Adam Reed <adam@secretcode.ninja>
2015-10-07 23:24:18 +1100

    Add options so that we can move the "snake" from front to back, or back to front, and add an example of the new front to back mode.

--- Snake/Snake.ino
+++ Snake/Snake.ino
@@ -23,6 +23,12 @@ rgb_t theColour = GREEN;
 // Define the delay between lighting up each LED
 int theDelay = 100;
 
+/*
+ * Don't change the following variables
+ */
+bool frontToBack = true;
+bool firstRun = true;
+
 void setup(void) {
   // Serial port options for control of the Cube using serial commands are:
   // 0: Control via the USB connector (most common).
@@ -46,8 +52,11 @@ void setup(void) {
 }
 
 void loop(void) {
-  snake(3, 3, 0, theColour);
-  snake(3, 3, 0, BLACK);
+  snake(3, 3, 3, theColour);
+  snake(3, 3, 3, BLACK);
+  
+  snake(0, 0, 0, theColour);
+  snake(0, 0, 0, BLACK);
 }
 
 void snake(byte xPos, byte yPos, byte zPos, rgb_t theColour)
@@ -66,6 +75,18 @@ void snake(byte xPos, byte yPos, byte zPos, rgb_t theColour)
     topToBottom = true;
   }
 
+  // Indicate whether the sname is moving front to back, or back to front
+  // - Y Direction (but only do this on the first time through this procedure)
+  if(firstRun) {
+    if (yPos == 3) {
+      // Move from the back to the front
+      frontToBack = false;
+    }
+
+    // Flag that we have set the Y direction, and it shouldn't be run again
+    firstRun = false;
+  }
+  
   // Counters to track our position for X and Z planes
   int xLoop = 0;
   int zLoop = 0;
@@ -123,11 +144,6 @@ void snake(byte xPos, byte yPos, byte zPos, rgb_t theColour)
     zLoop++;
   }
 
-  // Handle movement in the Y direction
-  if (yPos != 0) {
-    // We aren't at the front of the cube, so move forward
-    yPos--;
-
     // Ensure that when we are at the edge of the cube, the Z position
     // is correctly set for the next interation, and recursively call
     // the snake function to handle it.
@@ -136,7 +152,21 @@ void snake(byte xPos, byte yPos, byte zPos, rgb_t theColour)
     } else {
       zPos = 3;
     }
+
+  // Handle movement in the Y direction. taking caution to only call the code while
+  // still within the bounds of the cube
+  if (frontToBack && yPos != 3) {
+    // We aren't at the front of the cube, so move forward
+    yPos++;
+    snake(xPos, yPos, zPos, theColour);
+  } else if (!frontToBack && yPos != 0) {
+    yPos--;
     snake(xPos, yPos, zPos, theColour);
   }
+
+  // This point is only reached at the end of the sequence, so reset the firstRun
+  // flag and frontToBack so that if called again it is in a clean state
+  firstRun = true;
+  frontToBack = true;
 }
 
