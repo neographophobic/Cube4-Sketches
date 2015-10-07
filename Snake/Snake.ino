c5099b6d9b7ff8340c40d0d7d95024d052852ac8
Adam Reed <adam@secretcode.ninja>
2015-10-07 22:42:46 +1100

    Improve the comments in the snake function, and fix code formatting.

--- Snake/Snake.ino
+++ Snake/Snake.ino
@@ -52,33 +52,54 @@ void loop(void) {
 
 void snake(byte xPos, byte yPos, byte zPos, rgb_t theColour)
 {
-  // Track the direction the "Snake" moves
+  // Indicate whether the snake is moving left to right, or right to left
+  // - X Direction
   bool leftToRightMovement = false;
   if (xPos == 0) {
     leftToRightMovement = true;
   }
 
+  // Indicate whether the snake is moving top to bottom, or bottom to top
+  // - Z Direction
   bool topToBottom = false;
   if (zPos == 3) {
-     topToBottom = true;
+    topToBottom = true;
   }
-  
+
+  // Counters to track our position for X and Z planes
   int xLoop = 0;
   int zLoop = 0;
+
+  // Outer loop ensures we light up the LEDs from the top to bottom, or
+  // bottom to top, depending on our given "Z" direction information
   while (zLoop < 4)
   {
+
+    // The inner loop lights up a given LED, and ensures that all LEDs
+    // are lit up in its row based of the direction (X) we are moving
+    // one at a time until all 4 are lit.
     while (xLoop < 4)
     {
+      // Light up the LED
       cube.set(xPos, yPos, zPos, theColour);
+
+      // Wait the provided time
       delay(theDelay);
+
+      // Move to the next LED based off our X direction
       if (leftToRightMovement) {
-        xPos++;
+        xPos++; // Next LED to the right
       } else {
-        xPos--;
+        xPos--; // Next LED to the left
       }
+
+      // Increment loop counter
       xLoop++;
     }
 
+    // Ensure that when we are at the edge of the cube, the X position
+    // is correctly set for the next interation, and that we flip the
+    // left to right movement direction
     if (leftToRightMovement) {
       leftToRightMovement = false;
       xPos = 3;
@@ -86,23 +107,36 @@ void snake(byte xPos, byte yPos, byte zPos, rgb_t theColour)
       leftToRightMovement = true;
       xPos = 0;
     }
+
+    // Set the Z plane to work with based on the direction up or down we
+    // are moving
     if (topToBottom) {
       zPos--;
     } else {
       zPos++;
     }
+
+    // Reset the X Loop so that all 4 planes are lit up
     xLoop = 0;
+
+    // Increment loop counter
     zLoop++;
   }
+
+  // Handle movement in the Y direction
   if (yPos != 0) {
+    // We aren't at the front of the cube, so move forward
     yPos--;
+
+    // Ensure that when we are at the edge of the cube, the Z position
+    // is correctly set for the next interation, and recursively call
+    // the snake function to handle it.
     if (topToBottom) {
       zPos = 0;
-      snake(xPos, yPos, zPos, theColour);
     } else {
       zPos = 3;
-      snake(xPos, yPos, zPos, theColour);
     }
+    snake(xPos, yPos, zPos, theColour);
   }
 }
 
