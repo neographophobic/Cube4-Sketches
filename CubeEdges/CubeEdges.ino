7fb64a9e4169c8bc8cb6c45ab5b3e7968a6c30b1
Adam Reed <adam@secretcode.ninja>
2015-10-25 23:10:17 +1100

    Copy default template to start CubeEdges Sketch

---
+++ CubeEdges/CubeEdges.ino
@@ -0,0 +1,43 @@
+
+/*
+ * File:    CubeEdges.ino
+ * Version: 1.0
+ * Author:  Adam Reed (adam@secretcode.ninja)
+ * License: BSD 3-Clause Licence
+ */
+
+#include "SPI.h"
+#include "Cube.h"
+
+Cube cube;
+
+/*
+ * User editable variables
+ */
+
+void setup(void) {
+  // Serial port options for control of the Cube using serial commands are:
+  // 0: Control via the USB connector (most common).
+  // 1: Control via the RXD and TXD pins on the main board.
+  // -1: Don't attach any serial port to interact with the Cube.
+  cube.begin(0, 115200); // Start on serial port 0 (USB) at 115200 baud
+
+  // Wait for the serial interface, to be established, or for a maximum of
+  // 3 seconds.
+  byte waitCounter = 0;
+  while (waitCounter < 30 && !Serial) {
+    delay(100);
+    waitCounter++;
+  }
+
+  // Print Debug Info if a serial interface is present
+  if (Serial)
+  {
+    serial->println("Cube Edges v1.0");
+  }
+}
+
+void loop(void) {
+
+}
+
