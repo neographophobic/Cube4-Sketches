e0921a9c06e45865f86cd9d39a04287c8bf73ff5
Adam Reed <adam@secretcode.ninja>
2016-01-24 00:01:29 +1100

    Rename the function to draw the frame from sweep to drawSweepAnimationFrame

--- Sweep/Sweep.ino
+++ Sweep/Sweep.ino
@@ -16,18 +16,27 @@ void loop(void) {
   rgb_t theColour = BLUE;
   int theDelay = 75;
 
-  sweep(0, 0, 1, 1, 2, 2, 3, 3, theColour, theDelay);
-  sweep(0, 1, 1, 1, 2, 2, 3, 2, theColour, theDelay);
-  sweep(0, 2, 1, 2, 2, 1, 3, 1, theColour, theDelay);
-  sweep(0, 3, 1, 2, 2, 1, 3, 0, theColour, theDelay);
-  sweep(1, 2, 1, 3, 2, 0, 2, 1, theColour, theDelay);
-  sweep(1, 0, 1, 1, 2, 2, 2, 3, theColour, theDelay);
+  // Frame 1
+  drawSweepAnimationFrame(0, 0, 1, 1, 2, 2, 3, 3, theColour, theDelay);
 
+  // Frame 2
+  drawSweepAnimationFrame(0, 1, 1, 1, 2, 2, 3, 2, theColour, theDelay);
+
+  // Frame 3
+  drawSweepAnimationFrame(0, 2, 1, 2, 2, 1, 3, 1, theColour, theDelay);
+
+  // Frame 4
+  drawSweepAnimationFrame(0, 3, 1, 2, 2, 1, 3, 0, theColour, theDelay);
+
+  // Frame 5
+  drawSweepAnimationFrame(1, 2, 1, 3, 2, 0, 2, 1, theColour, theDelay);
+
+  // Frame 6
+  drawSweepAnimationFrame(1, 0, 1, 1, 2, 2, 2, 3, theColour, theDelay);
 }
 
-void sweep(byte x1, byte y1, byte x2, byte y2, byte x3, byte y3, byte x4, byte y4, rgb_t theColour, int theDelay) {
+void drawSweepAnimationFrame(byte x1, byte y1, byte x2, byte y2, byte x3, byte y3, byte x4, byte y4, rgb_t theColour, int theDelay) {
   cube.all(BLACK);
-
   cube.line(x1, y1, 0, x1, y1, 3, theColour);
   cube.line(x2, y2, 0, x2, y2, 3, theColour);
   cube.line(x3, y3, 0, x3, y3, 3, theColour);
