99d6f0b290edb25d0fc098a5ffa7f5ef17c0fe19
Adam Reed <adam@secretcode.ninja>
2016-01-26 00:30:39 +1100

    Improve the commenting in the sketch and run the auto code formatter.

--- RandomPull/RandomPull.ino
+++ RandomPull/RandomPull.ino
@@ -1,14 +1,23 @@
-
 /*
- * File:    RandomPull.ino
- * Version: 1.0
- * Author:  Adam Reed (adam@secretcode.ninja)
- * License: BSD 3-Clause Licence
- */
+   File:      RandomPull.ino
+   Purpose:   pattern for the Freetronics 4x4x4 Cube
+   Author:    Adam Reed (adam@secretcode.ninja)
+   Licence:   BSD 3-Clause Licence
+*/
 
-#include "SPI.h"
+// Include required libraries
+#include <SPI.h>
 #include "Cube.h"
 
+/*
+   User editable variables
+*/
+
+
+/*
+   Don't edit these variables
+*/
+// Create an instance of the cube class
 Cube cube;
 
 struct point
