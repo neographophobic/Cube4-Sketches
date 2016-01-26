04ac962fd5a77e0e1a34191da3ce7fb404bf3fb6
Adam Reed <adam@secretcode.ninja>
2016-01-26 13:14:59 +1100

    Add my standard print to serial on startup with the sketch name and version, bumping the version of this script as it's been previously released.

--- Letters/Letters.ino
+++ Letters/Letters.ino
@@ -27,6 +27,20 @@ void setup() {
   // -1: Don't attach any serial port to interact with the Cube.
   cube.begin(0, 115200); // Start on serial port 0 (USB) at 115200 baud
 
+  // Wait for the serial interface, to be established, or for a maximum of
+  // 3 seconds.
+  byte waitCounter = 0;
+  while (waitCounter < 30 && !Serial) {
+    delay(100);
+    waitCounter++;
+  }
+
+  // Print Debug Info if a serial interface is present
+  if (Serial)
+  {
+    serial->println("Letters v1.1");
+  }
+
   // Random Seed
   // Set this to a static number to get the same sequence each time it starts,
   // otherwise set it to analogRead(Unused Pin) to run a difference sequence
