ebefd771cbbf268c5471427f9dc2c45bf969cd32
Adam Reed <adam@secretcode.ninja>
2016-01-26 11:57:58 +1100

    Move the shuffle function to the Cube4_ARUtils library.

--- RandomPull/RandomPull.ino
+++ RandomPull/RandomPull.ino
@@ -221,26 +221,3 @@ void startPlane(byte axis, byte offset, rgb_t theColour, int theDelay)
   }
 }
 
-/*
-    Arrange the N elements of ARRAY in random order.
-    Only effective if N is much smaller than RAND_MAX;
-    if this may not be the case, use a better random
-    number generator.
-
-    Code from: http://benpfaff.org/writings/clc/shuffle.html
-
-*/
-void shuffle(int *array, size_t n)
-{
-  if (n > 1) {
-    size_t i;
-    for (i = 0; i < n - 1; i++) {
-      size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
-      int t = array[j];
-      array[j] = array[i];
-      array[i] = t;
-    }
-  }
-}
-
-
